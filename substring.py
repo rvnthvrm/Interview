23de1cd6edec92a80baab4fee6ef2edc4ad194a4
Revanth <rvnthvrm@gmail.com>
2020-04-03 16:35:56 +0530

    cleaning up

--- substring.py
+++ substring.py
@@ -1,12 +1,8 @@
 def substring(inp):
     result = [inp[i: j] for i in range(len(inp)) for j in range(i + 1, len(inp) + 1)]
-    final_out = []
-    for i in result:
-        if len(i) == len(set(i)):
-            final_out.append(i)
-
+    final_out = [i for i in result if len(i) == len(set(i))]
     o = list(set(final_out))
-    print('All Substrings: ', o)
+    print('All Substrings: ', sorted(o))
     print('Largest Substring: ', max(o, key=len))
 
 
